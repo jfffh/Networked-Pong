dec8505a7968098642265e9f0a24ef1a99cc5267
jfffh <157326539+jfffh@users.noreply.github.com>
2025-09-16 15:09:46 +0930

    Added binary message infrastructure
    Added binary messages and binary message handlers (bulk encryption/decryption)

---
+++ binary_messaging.py
@@ -0,0 +1,65 @@
+import struct
+
+class binary_message:
+    def __init__(self, type:str, ecryption_format:str):
+        self.type = type
+        self.binary_type = ord(self.type)
+
+        self.encryption_format = ecryption_format
+        self.encryption_format_with_message_header = "BL" + ecryption_format
+
+    def encrypt_into_binary(self, *args):
+        return struct.pack(self.encryption_format_with_message_header, self.binary_type, struct.calcsize(self.encryption_format), *args)
+    
+    def decrypt_full_message_from_binary(self, data, offset:int = 0):
+        datas = list(struct.unpack_from(self.encryption_format_with_message_header, data, offset))
+        datas.pop(0); datas.pop(0)
+        if len(datas) == 1:
+            return datas[0]
+        else:
+            return datas
+    
+    def decrypt_only_message_from_binary(self, data, offset:int = 0):
+        return struct.unpack_from(self.encryption_format, data, offset)
+
+class binary_message_handler:
+    def __init__(self, message_types:list[binary_message]):
+        self.message_types:dict[str:binary_message] = {}
+        for message in message_types:
+            self.message_types[message.type] = message
+
+    def encrypt_message(self, messages:list[tuple[str, tuple]]):
+        encoded_data = []
+        for message_type, message_data in messages:
+            encoded_data.append(self.message_types[message_type].encrypt_into_binary(*message_data))
+        return b"".join(encoded_data)
+    
+    def decrypt_message(self, data:bytes):
+        i = 0
+
+        message_header_length = struct.calcsize("BL")
+        message_full_length = len(data)
+
+        decrypted_data = []
+
+        while True:
+            if len(data) - i < message_header_length:
+                break
+
+            message_type, message_length = struct.unpack_from("BL", data, i)
+            message_type = chr(message_type)
+
+            i += message_header_length
+            if i >= message_full_length:
+                break
+
+            if message_type in self.message_types:
+                if len(data) - i < message_length:
+                    break
+            
+                decrypted_data.append(self.message_types[message_type].decrypt_only_message_from_binary(data, i))
+                i += message_length
+
+                if i >= message_full_length:
+                    break
+        return decrypted_data
