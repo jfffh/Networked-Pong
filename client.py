3dfaf6fe8e3e84a7bb978accb3fb02f82d1d85d9
jfffh <157326539+jfffh@users.noreply.github.com>
2025-09-24 11:04:30 +0930

    Testbed UDP server and client
    Added completed UDP server and client.

--- client.py
+++ client.py
@@ -6,8 +6,8 @@ import secrets
 import sys
 from binary_messaging import binary_message, binary_message_handler
 import networked_player
-from network_config import HOST, PORT
 import global_time
+from network_config import SERVER_IP, SERVER_TCP_PORT, SERVER_UDP_PORT
 
 class player(networked_player.player):
     def render(self, screen:pygame.Surface, color:tuple):
@@ -16,8 +16,8 @@ class player(networked_player.player):
         screen.blit(surface, surface.get_rect(center = (self.x, self.y)))
 
 class controllable_player(player):
-    def __init__(self, socket:socket.socket|None, id:int):
-        super().__init__(socket=socket, id=id)
+    def __init__(self, id:int):
+        super().__init__(id=id)
 
     def update(self, keys_held:dict, dt:float):
         if keys_held.get(pygame.K_UP, False) or keys_held.get(pygame.K_w, False):
@@ -34,24 +34,18 @@ class controllable_player(player):
 
     def render(self, screen:pygame.Surface):
         return super().render(screen, (255, 0, 0))
-    
-    def update_server(self):
-        global message_handler
-        try:
-            self.socket.sendall(message_handler.encrypt_message([
-                ("p", (self.id, round(self.x), round(self.y)))
-            ]))
-        except:
-            pass
 
 pygame.init()
 
 screen = pygame.display.set_mode((640, 360))
 clock = pygame.time.Clock()
 
+MY_IP = "127.0.0.1"
+AVAILABLE_PORTS = [port for port in range(62743, 65535)]
+
 def init():
     global my_player
-    my_player = controllable_player(None, int.from_bytes(secrets.token_bytes(3), byteorder=sys.byteorder))
+    my_player = controllable_player(int.from_bytes(secrets.token_bytes(3), byteorder=sys.byteorder))
 
     global run 
     run = True
@@ -59,33 +53,45 @@ def init():
     global message_handler
     message_handler = binary_message_handler([
         binary_message("p", "iii"),
+        binary_message("h", "ii"),
         binary_message("l", "d")
     ])
 
     global players 
     players = {}
 
-    global has_succesfully_connected_with_server
-    has_succesfully_connected_with_server = False
-
-    global trying_to_connect_to_server
-    trying_to_connect_to_server = False
-
     global keys_held
     keys_held = {}
 
-    global latency 
-    latency = None
+    global client_UDP_socket
+    client_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for port in AVAILABLE_PORTS:
+        try:
+            client_UDP_socket.bind((MY_IP, port))
+
+            global MY_UDP_PORT
+            MY_UDP_PORT = port
+        except:
+            pass
+    client_UDP_socket.settimeout(1)
+
+    global connected 
+    connected = False
+
+    global latency
+    latency = 0
 
 def start_threads():
     global thread_count
     thread_count = 0
 
-    threading.Thread(target=listen_to_server).start()
+    if connected:
+        threading.Thread(target=listen_to_server).start()
+        threading.Thread(target=communicate_with_server).start()
+        threading.Thread(target=check_for_timed_out_players).start()
 
 def main():
-    global screen, clock, my_player, run, keys_held, latency
-
+    global screen, clock, my_player, run, keys_held
     dt = 0
 
     while run:
@@ -100,14 +106,10 @@ def main():
                 keys_held[event.key] = True
             elif event.type == pygame.KEYUP:
                 keys_held[event.key] = False
-        
-        if keys_held.get(pygame.K_LCTRL, False) and keys_held.get(pygame.K_r, False) and has_succesfully_connected_with_server == False and trying_to_connect_to_server == False:
-            threading.Thread(target=listen_to_server).start()
 
         screen.fill((0, 0, 0))
         
         my_player.update(keys_held, dt)
-        my_player.update_server()
         my_player.render(screen)
             
         for player in players.values():
@@ -120,76 +122,86 @@ def main():
             fps = None
         else:
             fps = round(fps)
-        
-        pygame.display.set_caption("latency: " + str(latency) + " ms | fps: " + str(fps))
 
+        pygame.display.set_caption("latency: " + str(round(latency, 4)) + " ms | fps: " + str(fps))
+        
 def listen_to_server():
-    global run, thread_count, my_player, players, has_succesfully_connected_with_server, trying_to_connect_to_server, latency
+    global run, thread_count, my_player, players, client_UDP_socket, latency
 
     thread_count += 1
 
-    trying_to_connect_to_server = True
+    print("listening at " + MY_IP + ":" + str(MY_UDP_PORT))
 
-    my_player.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    buffer = b""
 
-    with my_player.socket:
-        my_player.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-        my_player.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-        my_player.socket.settimeout(1)
-        print("awaiting connection...")
-        try:
-            my_player.socket.connect((HOST, PORT))
-            print("connected to " + HOST + ":" + str(PORT))
-            has_succesfully_connected_with_server = True
-            trying_to_connect_to_server = False
-        except:
-            print("unable to connect to " + HOST + ":" + str(PORT))
-            trying_to_connect_to_server = False
-            thread_count -= 1
-            return
-                
-        time_since_last_message = 0
-
-        buffer = b""
-
-        start_ntp_time = global_time.time()
-        start_time = time.time()
+    start_ntp_time = global_time.time()
+    start_time = time.time()
         
-        while run:
-            try:
-                buffer = b"".join([my_player.socket.recv(4096)])
-                decrypted_messages, decrypted_data, decrypted_data_length = message_handler.decrypt_message(buffer)
-                if len(decrypted_messages) > 0:
-                    for message, data in zip(decrypted_messages, decrypted_data):
-                        if message == "p":
-                            player_id = data[0]
-                            if player_id in players:
-                                players[player_id].x, players[player_id].y = data[1], data[2]
-                            else:
-                                players[player_id] = player(id=player_id)
-                                players[player_id].x, players[player_id].y = data[1], data[2]
-                        if message == "l":
-                            latency = ((start_ntp_time + (time.time() - start_time)) - data) * 1000
-                    time_since_last_message = 0
-                    buffer = buffer[decrypted_data_length:]
-            except socket.timeout:
-                pass
-            except (ConnectionAbortedError, ConnectionResetError):
-                pass
-            time.sleep(0.001)
-            time_since_last_message += 0.001
-    
-            if time_since_last_message > 10:
-                break
-                
+    while run:
         try:
-            my_player.socket.shutdown(0)
-        except:
-            pass
+            data, address = client_UDP_socket.recvfrom(4096)
+            if address != (MY_IP, MY_UDP_PORT):
+                buffer = b"".join([buffer, data])
+        except socket.timeout:
+            continue
+        except (ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError):
+            continue
+            
+        decrypted_messages, decrypted_data, decrypted_data_length = message_handler.decrypt_message(buffer)
+        if len(decrypted_messages) > 0:
+            for message, data in zip(decrypted_messages, decrypted_data):
+                if message == "p":
+                    if data[0] in players:
+                        pass
+                    else:
+                        players[data[0]] = player(None, data[0])
+                    players[data[0]].x, players[data[0]].y = data[1], data[2]
+                    players[data[0]].last_message = time.time()
+                elif message == "l":
+                    latency = abs(((start_ntp_time + (time.time() - start_time)) - data[0]) * 1000)
+            
+            buffer = buffer[decrypted_data_length:]
 
-        print("connection with " + HOST + ":" + str(PORT) + " terminated")
-        has_succesfully_connected_with_server = False
-    
+    print("stopped listening at " + MY_IP + ":" + str(MY_UDP_PORT))
+                    
+    thread_count -= 1
+    return
+
+def communicate_with_server():
+    global run, thread_count, my_player, players, client_UDP_socket
+
+    thread_count += 1
+
+    print("sending data though " + MY_IP + ":" + str(MY_UDP_PORT))
+
+    while run:
+        try:
+            client_UDP_socket.sendto(
+                message_handler.encrypt_message([("p", (my_player.id, round(my_player.x), round(my_player.y)))]),
+                (SERVER_IP, SERVER_UDP_PORT)
+            )
+            time_since_last_message = 0
+        except socket.timeout:
+            pass
+        except (ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError):
+            continue
+
+    print("stopped sending data though " + MY_IP + ":" + str(MY_UDP_PORT))
+                            
+    thread_count -= 1
+    return
+
+def check_for_timed_out_players():
+    global run, thread_count, players
+
+    thread_count += 1
+
+    while run:
+        for player_id, player in players.copy().items():
+            if time.time() - player.last_message > 10:
+                del players[player_id]
+        time.sleep(0.1)
+
     thread_count -= 1
     return
 
@@ -198,7 +210,39 @@ def await_threads():
     while thread_count > 0:
         time.sleep(0.001)
 
+    global client_UDP_socket
+    try:
+        client_UDP_socket.shutdown(0)
+    except:
+        client_UDP_socket.close()
+    finally:
+        pass
+
+def attempt_tcp_handshake():
+    global connected, my_player
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_TCP_socket:
+        client_TCP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        client_TCP_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
+        client_TCP_socket.settimeout(1)
+        print("awaiting tcp handshake...")
+        try:
+            client_TCP_socket.connect((SERVER_IP, SERVER_TCP_PORT))
+            connected = True
+        except:
+            print("unable to establish TCP handshake with " + SERVER_IP + ":" + str(SERVER_UDP_PORT))
+            return
+        
+        client_TCP_socket.sendall(
+            message_handler.encrypt_message([("h", (my_player.id, MY_UDP_PORT))])
+        )
+        
+        print("handshake with " + SERVER_IP + ":" + str(SERVER_TCP_PORT) + " completed")
+    
+    return
+        
 init()
+attempt_tcp_handshake()
 start_threads()
 main()
 await_threads()
